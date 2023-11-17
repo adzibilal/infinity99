dd803944378386569febd7453a0778cbd5b1aac4
adzibilal <adzibilal02@gmail.com>
2023-11-17 16:10:59 +0700

    change price and about us

--- js/dataMenu.js
+++ js/dataMenu.js
@@ -1,85 +1,85 @@
 const Menu = [
     {
         name: "Cappucino",
-        price: 23000,
+        price: '-',
         image: "/img/menu/menu1.jpg",
         category: "Coffee"
     },
     {
         name: "Espresso",
-        price: 20000,
+        price: '-',
         image: "/img/menu/menu2.jpg",
         category: "Coffee"
     },
     {
         name: "Latte",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu3.jpg",
         category: "Coffee"
     },
     {
         name: "Mojito Strawberry",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu4.jpg",
         category: "Mojito"
     },
     {
         name: "Mojito Lime",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu5.jpg",
         category: "Mojito"
     },
     {
         name: "Mojito Oragnge",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu8.jpg",
         category: "Mojito"
     },
     {
         name: "Taro",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu6.jpg",
         category: "Smoothies"
     },
     {
         name: "Green Tea",
-        price: 25000,
+        price: '-',
         image: "/img/menu/menu7.jpg",
         category: "Smoothies"
     },
     {
         name: "Onion Ring",
-        price: 25000,
+        price: '-',
         image: "/img/menu/onion.jpg",
         category: "Snack"
     },
     {
         name: "French Fries",
-        price: 25000,
+        price: '-',
         image: "/img/menu/kentang.jpg",
         category: "Snack"
     },
     {
         name: "Sausage",
-        price: 25000,
+        price: '-',
         image: "/img/menu/sosis.jpg",
         category: "Snack"
     },
     {
         name: "Pisang Keju",
-        price: 25000,
+        price: '-',
         image: "/img/menu/pisang-keju.jpg",
         category: "Snack"
     },
     {
         name: "Nasi Goreng",
-        price: 25000,
+        price: '-',
         image: "/img/menu/nasgor.jpg",
         category: "Main Course"
     },
     {
         name: "Ramen Katsu",
-        price: 25000,
+        price: '-',
         image: "/img/menu/ramenkatsu.jpg",
         category: "Main Course"
     },
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,4 @@
 export function rupiahFormat(price) {
+    if (price === "-") return price;
     return "Rp " + price.toLocaleString("id-ID");
 }
